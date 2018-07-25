c432a5ca04ec758ae97473b0b4f7df3bf4750a34
Hiromi ISHII <konn.jinro@gmail.com>
2018-07-26 01:25:18 +0900

    Waiting mechanism

--- src/Main.hs
+++ src/Main.hs
@@ -5,6 +5,7 @@ module Main where
 import           Control.Arrow          ((>>>))
 import           Control.Concurrent
 import           Control.Exception
+import qualified Control.Foldl          as F
 import           Control.Lens
 import           Control.Monad
 import           Control.Monad.IO.Class
@@ -15,7 +16,6 @@ import qualified Data.ByteString.Char8  as BS
 import qualified Data.ByteString.Lazy   as LBS
 import           Data.ByteString.Lens
 import           Data.Maybe             (fromJust)
-import           Data.Monoid
 import qualified Data.Text.Encoding     as T
 import           GHC.Generics           (Generic)
 import           Language.Haskell.Exts
@@ -27,6 +27,7 @@ import           Streaming.Concurrent
 import qualified Streaming.Prelude      as S
 import           System.Clock
 import           System.IO
+import           System.Random
 
 newtype AccTok  = AccTok { accessToken :: ByteString }
   deriving (Read, Show, Eq, Ord)
@@ -35,42 +36,57 @@ instance FromJSON AccTok where
   parseJSON = withObject "object" $ \dic ->
     AccTok . T.encodeUtf8 <$> dic .: "access_token"
 
+wait :: Int -> IO ()
+wait i0 = do
+  i <- floor . (fromIntegral i0 *) <$> randomRIO (0.75, 1.25 :: Double)
+  -- hPutStrLn stderr $ "Waiting " ++ show (i `div` 10^3) ++ " millisecs"
+  threadDelay i
+
+aggr :: F.Fold (String, [Decl SrcSpanInfo]) (Int, Int, [String], [Decl SrcSpanInfo])
+aggr = (,,,) <$> F.length
+             <*> F.handles (F.filtered (not . null . snd)) F.length
+             <*> F.handles (F.filtered (not . null . snd) . _1) F.list
+             <*> F.handles _2 F.mconcat
+
 main :: IO ()
 main = do
+  hSetBuffering stderr =<< hGetBuffering stdout
   Just (AccTok tok) <- decodeFileStrict' "token.yaml"
   let github = oauth2Token tok
       opts = defaults & auth ?~ github
                       & param "q" .~ ["instance Generic extension:hs extension:lhs"]
-  i <- withStreamMapM 5 maybeParseModule (crawl opts "https://api.github.com/search/code") $
-       S.catMaybes >>> S.toList_
-  print i
+  (total, positive, posMods, decs) S.:> () <-
+    withStreamMapM 5 maybeParseModule (crawl opts "https://api.github.com/search/code") $
+    S.catMaybes >>> F.purely S.fold aggr
   putStrLn $ unwords [ "There are"
-                     , show $ getSum $ foldMap (Sum . length . snd) i
+                     , show $ length decs
                      , "custom instances."
                      ]
-  putStrLn $ unwords [ show $ length $ filter (not . null . snd) i
+  putStrLn $ unwords [ show positive
                      , "out of"
-                     , show $ length i
-                     , "modules have custom instances."
+                     , show total
+                     , "modules have custom instances:"
                      ]
+  mapM_ putStrLn posMods
 
 maybeParseModule :: String -> IO (Maybe (String, [Decl SrcSpanInfo]))
 maybeParseModule url = do
   rsp <- get' url
-  hPutStrLn stderr $ "Processed: " ++ url
+  -- hPutStrLn stderr $ "Processed: " ++ url
   let src = rsp ^. unpackedChars
-  return $ getCustomGeneric src
+  return $ (,) url <$> getCustomGeneric src
 
-getCustomGeneric :: String -> Maybe (String, [Decl SrcSpanInfo])
+getCustomGeneric :: String -> Maybe [Decl SrcSpanInfo]
 getCustomGeneric src = do
   i <- maybeResult $ parseModuleWithMode defaultParseMode { extensions = glasgowExts } src
-  return (src, extractCustomGeneric i)
+  return $ extractCustomGeneric i
 
 
 get' :: String -> IO LBS.ByteString
 get' =
   untilJust . fmap eitherMaybe . try @HttpException . \url ->
-    do hPutStrLn stderr $ "accessing: " ++ url; threadDelay (10^6); view responseBody <$> get url
+    do -- hPutStrLn stderr $ "accessing: " ++ url;
+       wait (10^6); view responseBody <$> get url
 
 eitherMaybe :: Either HttpException LBS.ByteString -> Maybe LBS.ByteString
 eitherMaybe (Right a) = Just a
@@ -122,28 +138,28 @@ crawl opt = loop
       case er of
         Left (HttpExceptionRequest _ (StatusCodeException r _)) -> do
           let dur = r ^?! responseHeader "Retry-After" . unpackedChars . to read
-          liftIO $ threadDelay $ dur * 10^6
+          liftIO $ wait $ dur * 10^6
           loop url
-        Left{} -> liftIO (threadDelay (10^6)) >> loop url
+        Left{} -> liftIO (wait (10^6)) >> loop url
         Right r ->
           forM_ (decode =<< r ^? responseBody) $ \(Items is) -> do
             S.each (map rawUrl is)
             forM_ (r ^? responseLink "rel" "next" . linkURL . unpackedChars) $ \l -> do
-              liftIO $ threadDelay . (* 10^6) =<< calcWait r
+              liftIO $ wait . (* 10^6) =<< calcWait r
               loop l
 
 calcWait ::  Response body -> IO Int
 calcWait rsp = do
   let remain = rsp ^?! responseHeader "X-RateLimit-Remaining" . unpackedChars . to read
       reset  = rsp ^?! responseHeader "X-RateLimit-Reset" . unpackedChars . to read
-      retry  = rsp ^? responseHeader "Retry-After" . unpackedChars . to read
+      retry  = rsp ^?  responseHeader "Retry-After" . unpackedChars . to read
   now <- (`div` 10^9) . toNanoSecs <$> getTime Realtime
   case retry of
     Just ret -> return ret
     Nothing -> do
       let durSecs = reset - now
-          wait = ceiling (fromInteger remain / fromInteger durSecs :: Double)
-      return $ max 1 $ if durSecs > 0 then wait else 0
+          w = ceiling (fromInteger remain / fromInteger durSecs :: Double)
+      return $ max 1 $ if durSecs > 0 then w else 0
 
 extractCustomGeneric :: Module l -> [Decl l]
 extractCustomGeneric (Module _ _ _ _ decs) =
